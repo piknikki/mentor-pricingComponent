04bca6ced0c0a3477776e6a980b32751e615b1a4
Nikki Petersen <piknik@me.com>
2020-09-10 09:38:55 -0600

    push the rest of the biz

---
+++ src/components/Pricing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img class="logo" alt="Aroma Therapy Logo" src="./assets/aromatherapy.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pricing'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'IBM Plex Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-image: url("./src/assets/bg-top.svg");
+  margin-top: 60px;
+}
+</style>
